c5ccf17631a0eebd5613b62932bd9d86f2ff5cdf
Preathiksha <109804004+bangeraprathiksha@users.noreply.github.com>
2026-07-23 12:10:58 +0530

    Create cpu_test.sv

---
+++ cpu_test.sv
@@ -0,0 +1,24 @@
+class cpu_test extends uvm_test;
+
+  `uvm_component_utils(cpu_test)
+
+  cpu_env env;
+
+  function new(string name = "cpu_test", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = cpu_env::type_id::create("env", this);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    cpu_sequence seq;
+    phase.raise_objection(this);
+    seq = cpu_sequence::type_id::create("seq");
+    seq.start(env.active_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+
+endclass
